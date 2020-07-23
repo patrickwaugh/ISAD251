86f6b251258cbbfcc75e10d4bacf720893ce8783
patrickwaugh <patrickwaugh510@protonmail.com>
2020-07-23 15:54:34 +0100

    Generated Models and Controllers from the database

---
+++ API/ISAD251/ISAD251_Referral/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ISAD251_Referral.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Appointment = new HashSet<Appointment>();
+            Deadline = new HashSet<Deadline>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public bool IsParent { get; set; }
+
+        public virtual ICollection<Appointment> Appointment { get; set; }
+        public virtual ICollection<Deadline> Deadline { get; set; }
+    }
+}
