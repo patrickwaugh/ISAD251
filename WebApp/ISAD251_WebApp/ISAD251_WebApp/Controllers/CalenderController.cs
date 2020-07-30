4b4537badae26a3bfe2d7619d45ae2fd21364f03
patrickwaugh <patrickwaugh510@protonmail.com>
2020-07-30 13:48:54 +0100

    Started on calender generation

---
+++ WebApp/ISAD251_WebApp/ISAD251_WebApp/Controllers/CalenderController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ISAD251_WebApp.Models;
+
+namespace ISAD251_WebApp.Controllers
+{
+    public class CalenderController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CalenderController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Calender()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- WebApp/ISAD251_WebApp/ISAD251_WebApp/Startup.cs
+++ WebApp/ISAD251_WebApp/ISAD251_WebApp/Startup.cs
@@ -24,6 +24,7 @@ namespace ISAD251_WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
